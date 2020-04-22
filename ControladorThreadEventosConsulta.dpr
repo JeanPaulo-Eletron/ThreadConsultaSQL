f2e63a720262b56cef7aeb149241c92dc114809d
jeanpauloriopreto@gmail.com <JeanPaulo-Eletron>
2020-04-22 20:56:08 -0300

    V2.11.5

--- ControladorThreadEventosConsulta.dpr
+++ ControladorThreadEventosConsulta.dpr
@@ -2,13 +2,16 @@ program ControladorThreadEventosConsulta;
 
 uses
   Vcl.Forms,
-  Main in 'Main.pas' {Form1};
+  Main in 'Main.pas' {Form1},
+  Vcl.Themes,
+  Vcl.Styles;
 
 {$R *.res}
 
 begin
   Application.Initialize;
   Application.MainFormOnTaskbar := True;
+  TStyleManager.TrySetStyle('Sapphire Kamri');
   Application.CreateForm(TForm1, Form1);
   Application.Run;
 end.
